b86c4768b19113fed56af7a7b41c5c2f166bcddb
Larionov Vadim <larionov.vadim@mail.ru>
2015-04-12 23:43:38 +0300

    Added library javax.comm.*

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,43 @@
-public class Main {
+import javax.comm.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.*;
+
+
+class Main {
+    static Enumeration portList;
+    static CommPortIdentifier portId;
+    static String messageString = "Hello, world!\n";
+    static SerialPort serialPort;
+    static OutputStream outputStream;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        portList = CommPortIdentifier.getPortIdentifiers();
+        System.out.println(portList.hasMoreElements());
+
+        while (portList.hasMoreElements()) {
+            portId = (CommPortIdentifier) portList.nextElement();
+            if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+                if (portId.getName().equals("COM1")) {
+                    //if (portId.getName().equals("/dev/term/a")) {
+                    try {
+                        serialPort = (SerialPort)
+                                portId.open("SimplseWriteApp", 2000);
+                    } catch (PortInUseException e) {}
+                    try {
+                        outputStream = serialPort.getOutputStream();
+                    } catch (IOException e) {}
+                    try {
+                        serialPort.setSerialPortParams(9600,
+                                SerialPort.DATABITS_8,
+                                SerialPort.STOPBITS_1,
+                                SerialPort.PARITY_NONE);
+                    } catch (UnsupportedCommOperationException e) {}
+                    try {
+                        outputStream.write(messageString.getBytes());
+                    } catch (IOException e) {}
+                }
+            }
+        }
     }
 }
